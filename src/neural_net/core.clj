88ba536e5fb4c05222e060fe0afc19fa3a7308bf
James <jmborden@gmail.com>
2017-01-06 11:56:18 -0600

    capture screen shot and write to file

--- src/neural_net/core.clj
+++ src/neural_net/core.clj
@@ -1,17 +1,19 @@
 (ns neural-net.core
-  (:require [neural-net.mame :as mame]
-            [neural-net.keyboard :as keyboard]))
+  (:require [neural-net.keyboard :as keyboard]
+            [neural-net.mame :as mame]
+            [neural-net.screen :as screen]))
 
 ;; note: first time running rom you will be prompted with to type "OK"
 (defn simulate
   []
   (future (mame/launch-rom "mrdo"))
-  (Thread/sleep 1500) ;; this should correspond to launch time
+  (Thread/sleep 2000) ;; this should correspond to launch time
   (keyboard/keytype "S") ;; just to start the game
   (Thread/sleep 400) ;; the time it takes to start the games
   (keyboard/keytype "5") ;; insert coint
   (keyboard/keytype "1") ;; start game
   (Thread/sleep 1000)
+  (screen/screen-grab "mrdo.png") ;; capture a screen shot
   (keyboard/hit-esc) ;; quit the game
   )
 
---
+++ src/neural_net/screen.clj
@@ -0,0 +1,16 @@
+(ns neural-net.screen
+  (:import (java.awt Robot Rectangle Toolkit)
+           (java.awt.image BufferedImage)
+           (java.io File IOException)
+           (javax.imageio ImageIO)))
+
+;; see http://www.mailsend-online.com/blog/screen-captures-with-java-and-clojure.html
+;; for a pure java implementation
+
+;; from http://gettingclojure.wikidot.com/cookbook:system
+(defn screen-grab [file-name]
+  (let [img-type (second (re-find (re-matcher #"\.(\w+)$" file-name)))
+        capture (.createScreenCapture (Robot.)
+                                      (Rectangle. (.getScreenSize (Toolkit/getDefaultToolkit))))
+        file (File. file-name)]
+    (ImageIO/write capture img-type file)))
