e03aafb36449e2b01fbac978d750882720e6e0b9
James <jmborden@gmail.com>
2017-01-09 13:09:30 -0600

    Mr. Do moves to the left of the screen and releases power ball

--- src/neural_net/core.clj
+++ src/neural_net/core.clj
@@ -17,16 +17,41 @@
   (reset! score (tesseract/read-score "score.png")))
 
 ;; note: first time running rom you will be prompted with to type "OK"
+;; note: 1.  would eventually be better to wait for particular cues
+;;       and then start the game.
+;;       e.g. wait until the prompt screen shows and include test for the "OK"
+;;            cue.
+;;            wait till the screen has a particular image. Would be
+;;            good to take screen shots and isolate a particular part of it.
+;;
+;;       2. Would be GREAT to make this headless
+;;       3. EVEN better to enable a speedup of the game
+;;          mame options: -[no]throttle
+;;                         -speed <factor>
 (defn simulate
   []
   (future (mame/launch-rom "mrdo"))
   (Thread/sleep 2000) ;; this should correspond to launch time
-  (keyboard/keytype "S") ;; just to start the game
-  (Thread/sleep 400) ;; the time it takes to start the games
-  (keyboard/keytype "5") ;; insert coint
-  (keyboard/keytype "1") ;; start game
-  ;;(Thread/sleep 1000)
-  (at-at/every 500 capture-score thread-pool)
-  ;;(keyboard/hit-esc) ;; quit the game
+  (keyboard/keytype! "S") ;; just to start the game
+  (Thread/sleep 500) ;; the time it takes to start the games
+  (keyboard/keytype! "5") ;; insert coint
+  (keyboard/keytype! "1") ;; start game
+  (Thread/sleep 1500)
+  (keyboard/press-key! :left)
+  (Thread/sleep 200)
+  (keyboard/release-key! :left)
+  (keyboard/press-key! :up)
+  (Thread/sleep 2000)
+  (keyboard/release-key! :up)
+  (keyboard/press-key! :up)
+  (keyboard/press-key! :left)
+  (Thread/sleep 2000)
+  (keyboard/release-key! :up)
+  (keyboard/release-key! :left)
+  (keyboard/hit-key! :right)
+  (keyboard/hit-key! :ctrl)
+  (Thread/sleep 2000)
+  (keyboard/hit-key! :esc) ;; quit the game
+  ;;(at-at/every 500 capture-score thread-pool)
   )
 
--- src/neural_net/keyboard.clj
+++ src/neural_net/keyboard.clj
@@ -2,6 +2,13 @@
   (:import java.awt.Robot
            java.awt.event.KeyEvent))
 
+;; It would be better if we had ONE robot object and designed an interface to it
+;; then all keys would be done through the robot obj instead of creating new
+;; ones for EACH key event
+
+;; perhaps something along the lines the clj-webdriver selenium wrapper?
+(def robot (new Robot))
+
 ;; from http://rosettacode.org/wiki/Simulate_input/Keyboard#Clojure
 ;; just a start, only does a-z,A-Z,0-9 does NOT handle special chars like ()
 ;; perhaps a delay would also help the output be more natural
@@ -39,32 +46,60 @@
 ;; https://github.com/calebmpeterson/awtbot
 ;;
 
-(defn keytype
+;; It would be better if we had ONE robot object and designed an interface to it
+;; then all keys would be done through the robot obj instead of creating new
+;; ones for EACH key event
+
+(defn keytype!
   "Given a string consisting only of a-z,A-Z,0-9, type them to the keyboard"
   [str]
-  (let [robot (new Robot)]
-    (doseq [ch str] 
-      (if (Character/isUpperCase ch)
+  (doseq [ch str]
+    (if (Character/isUpperCase ch)
+      (doto robot
+        (.keyPress (. KeyEvent VK_SHIFT))
+        (.keyPress (int ch))
+        (.delay 50)
+        (.keyRelease (int ch))
+        (.keyRelease (. KeyEvent VK_SHIFT)))
+      (let [upCh (Character/toUpperCase ch)]
         (doto robot
-          (.keyPress (. KeyEvent VK_SHIFT))
-          (.keyPress (int ch))
+          (.keyPress (int upCh))
           (.delay 50)
-          (.keyRelease (int ch))
-          (.keyRelease (. KeyEvent VK_SHIFT)))
-        (let [upCh (Character/toUpperCase ch)]
-          (doto robot 
-            (.keyPress (int upCh))
-            (.delay 50)
-            (.keyRelease (int upCh))))))))
+          (.keyRelease (int upCh)))))))
 
-(defn hit-esc
-  "Hit the escape button"
-  []
-  (let [robot (new Robot)]
+;; see https://docs.oracle.com/javase/7/docs/api/java/awt/event/KeyEvent.html
+;; Some key codes:
+;; VK_UP, VK_DOWN, VK_LEFT, VK_RIGHT, VK_CONTROL, VK_ESCAPE, VK_ALT,
+;; VK_ENTER
+(def key->key-event
+  {:up    (. KeyEvent VK_UP)
+   :down  (. KeyEvent VK_DOWN)
+   :left  (. KeyEvent VK_LEFT)
+   :right (. KeyEvent VK_RIGHT)
+   :ctrl  (. KeyEvent VK_CONTROL)
+   :alt   (. KeyEvent VK_ALT)
+   :esc   (. KeyEvent VK_ESCAPE)})
+
+(defn hit-key!
+  "Given a key-code represented in key->key-event, hit it. This means
+  to quickly press and release a key"
+  [key-code]
+  (let [delay 50]
     (doto robot
-      (.keyPress (. KeyEvent VK_ESCAPE))
-      (.delay 50)
-      (.keyRelease (. KeyEvent VK_ESCAPE)))))
+      (.keyPress (key-code key->key-event))
+      (.delay delay)
+      (.keyRelease (key-code key->key-event)))))
 
-;; you can delay executio of the thread and type in another window:
+(defn press-key!
+  "Given a key-code reprensented in key->key-event, press it"
+  [key-code]
+  (doto robot
+    (.keyPress (key-code key->key-event))))
+
+(defn release-key!
+  [key-code]
+  (doto robot
+    (.keyRelease (key-code key->key-event))))
+
+;; you can delay execution of the thread and type in another window:
 ;; (do (Thread/sleep 3000) (keytype "1234567890"))
