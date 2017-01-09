2e421752545b60411cde2e31dc65b0166665ccb1
James <jmborden@gmail.com>
2017-01-09 12:05:18 -0600

    score is read into atom while playing

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A Clojure Neural Net for playing games"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [overtone/at-at "1.2.0"]])
--- src/neural_net/core.clj
+++ src/neural_net/core.clj
@@ -1,7 +1,20 @@
 (ns neural-net.core
   (:require [neural-net.keyboard :as keyboard]
             [neural-net.mame :as mame]
-            [neural-net.screen :as screen]))
+            [neural-net.screen :as screen]
+            [neural-net.tesseract :as tesseract]
+            [overtone.at-at :as at-at]))
+
+(def score (atom 0))
+
+(def thread-pool (at-at/mk-pool))
+
+(defn capture-score
+  "Capture the score has the game is playing"
+  []
+  (screen/score-grab "score.png")
+  (println (tesseract/read-score "score.png"))
+  (reset! score (tesseract/read-score "score.png")))
 
 ;; note: first time running rom you will be prompted with to type "OK"
 (defn simulate
@@ -12,8 +25,8 @@
   (Thread/sleep 400) ;; the time it takes to start the games
   (keyboard/keytype "5") ;; insert coint
   (keyboard/keytype "1") ;; start game
-  (Thread/sleep 1000)
-  (screen/score-grab "score.png") ;; capture the score
-  (keyboard/hit-esc) ;; quit the game
+  ;;(Thread/sleep 1000)
+  (at-at/every 500 capture-score thread-pool)
+  ;;(keyboard/hit-esc) ;; quit the game
   )
 
