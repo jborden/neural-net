f5c54b4b5c1333d9417d7bfd99abdcf9960a4e2e
James <jmborden@gmail.com>
2017-01-16 15:13:10 -0600

    lr-cost-function added

--- src/neural_net/percepton.clj
+++ src/neural_net/percepton.clj
@@ -44,19 +44,33 @@
 ;; where
 ;; X(:,1) = (m/slice X 1 0)
 ;; X(:,2) = (m/slice X 1 1)
-
+;;
 ;; below would be a MUCH nicer syntax
 ;; (> (sin (+ (X : 1) (X : 2)) 0))
 ;; : = row-count in 1st position
 ;;   = column-count in 2nd position
 ;;
 ;; (m/array (map #(Math/sin %) (range 1 21))
+;; note: below is for a small matrix, see the comment about performance
+;; in core.matrix. Basically, because the operation is O(N), it suggests
+;; using a mutable array with mset! instead of mset
+;; temp = [0.25 0.5 -0.5]' = (m/array (mapv vector [0.25 0.5 -0.5]))
+;; temp(1) = 0; =  (m/mset (m/array (mapv vector [0.25 0.5 -0.5])) 0 0 0)
+;; J_reg = (temp' * temp) * (lambda / (2 * m)); =
+;; (* (m/select (m/mmul (m/transpose temp) temp) 0 0) (/ lambda (* 2 m)))
+;; note: above result should be a scalar, but core.matrix returns
+;; a single row matrix. m/select is used to get at this scalar
+
 
 ;; what? vectors have a row-count but not a column count? seems like bad aesthetics
 ;; (m/column-count the-vector) =>
 ;; RuntimeException Vector does not have dimension: 1  mikera.vectorz.matrix-api/eval20433/fn--20442 (matrix_api.clj:506)
 ;;
 ;; (m/row-count the-vector) => 20
+
+;; another problem: You have to know when a calculation will return a scalar,
+;; this isn't handed automatically. so when you get a #vectorz/matrix [[0.7670870967747265]],
+;; you have to do a (m/select <matrix> 0 0) on it to get the scalar out
 
 (defn sigmoid [z]
   (/ 1 (+ 1 (Math/exp (* -1 z)))))
@@ -131,3 +145,45 @@
 (def y
   (m/gt (m/reshape (m/emap #(Math/sin %) (op/+ (m/slice X 1 0) (m/slice X 1 1)))
                    [(m/row-count (m/slice X 1 0)) 1]) 0))
+
+(defn lr-cost-function
+  [theta X y lambda]
+  (let [m (m/row-count y)
+        ;; note: if very large theta arrays are being used, this might
+        ;; need to be converted to using mset!
+        ;;
+        ;; The corresponding octave/matlab code is given in comments
+        temp (m/mset theta 0 0 0) ;; temp = theta; temp(1) = 0;
+        J_reg (* (m/select (m/mmul (m/transpose temp) temp) 0 0)
+                 (/ lambda (* 2 m)))
+        ;; matlab = (temp' * temp) * (lambda / (2 * m));
+        J_factor (- (/ 1 m)) ;; -(1 / m)
+        J_term1 (m/select (m/mmul (m/emap #(Math/log %)
+                                          (m/emap sigmoid
+                                                  (m/mmul (m/transpose theta)
+                                                          (m/transpose X))))
+                                  y) 0 0)
+        ;; matlab = (log(sigmoid(theta' * X')) * y)
+        J_term2 (m/emap #(Math/log %)
+                        (m/emap #(- 1 %)
+                                (m/emap sigmoid
+                                        (m/mmul (m/transpose theta)
+                                                (m/transpose X)))))
+        ;; matlab = log(1 - sigmoid(theta' * X'))
+        J_term3 (m/emap #(- 1 %) y) ; (1 - y)
+        J (+ (* J_factor
+                (+ J_term1 (m/select (m/mmul J_term2 J_term3) 0 0)))
+             J_reg)
+        ;; matlab = (-(1 / m) * ((log(sigmoid(theta' * X')) * y) + log(1 - sigmoid(theta' * X'))  * (1 - y))) + J_reg;
+        ;; gradient
+        grad (op/+
+              (op/* (/ 1 m) (m/mmul (m/transpose X)
+                                    (m/sub (m/emap sigmoid (m/mmul X theta)) y)))
+              (op/* temp (/ lambda m)))
+        ;; matlab
+        ;; grad = (1 / m) * (X' * (sigmoid(X * theta) - y));
+        ;; temp = temp .* (lambda/m);
+        ;; grad = grad + temp;
+        ]
+    {:J J
+     :grad grad}))
